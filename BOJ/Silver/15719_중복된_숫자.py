883e74faa1e7a647714acaa33573bd9ee2b104e3
kyeonghwiPark <115651808+kyeonghwi@users.noreply.github.com>
2026-03-12 16:52:04 +0900

    Add script to compute difference of sums
    Implement a script to calculate the difference between the sum of input numbers and the sum of the first N natural numbers.

---
+++ BOJ/Silver/15719_중복된_숫자.py
@@ -0,0 +1,17 @@
+import sys
+N = int(input())
+sumN = sum(range(1, int(N)))
+numbers = sys.stdin.readline().rstrip()
+sumNum = 0
+temp = ""
+for num in numbers:
+    if num.isdigit():
+        temp += num
+    else:
+        sumNum += int(temp)
+        temp = ""
+
+# 마지막 숫자의 처리
+sumNum += int(temp)
+
+print(sumNum - sumN)
